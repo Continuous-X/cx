f733a5b9082becde602114cc933e08ffe6b0c202
Wolver Minion <mikeschmid71@googlemail.com>
2020-10-28 13:38:18 +0100

    Update version.go

--- pkg/version.go
+++ pkg/version.go
@@ -1,3 +1,20 @@
-package pkg
+package version
+
+var (
+	// commitFromGit is a constant representing the source version that
+	// generated this build. It should be set during build via -ldflags.
+	commitFromGit string
+	// versionFromGit is a constant representing the version tag that
+	// generated this build. It should be set during build via -ldflags.
+	versionFromGit = "unknown"
+	// major version
+	majorFromGit string
+	// minor version
+	minorFromGit string
+	// build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+	buildDate string
+	// state of git tree, either "clean" or "dirty"
+	gitTreeState string
+)
 
 

20015ac771e4985ed8a8e8d056d3bff20addd47e
Wolver Minion <mikeschmid71@googlemail.com>
2020-11-04 15:03:31 +0100

    Update metricGHRepoProtection.go

--- pkg/metrics/metricGHRepoProtection.go
+++ pkg/metrics/metricGHRepoProtection.go
@@ -1,15 +1,55 @@
 package metrics
 
-import "errors"
+import (
+	"cx-installer/pkg/constants"
+)
 
-type MetricGHProtection struct {
-	BranchName string
+const (
+	tagKeyPrefix = "tag_"
+	
+	dataKeyInstallationCxVersion          = "cx_version"
+	dataKeyInstallationCxCommand          = "cx_command"
+	dataKeyInstallationCxStatus           = "cx_status"
+	dataKeyInstallationCxConfigRepository = "cx_config_repository"
+	dataKeyInstallationOpenshiftNamespace = "openshift_namespace"
+	
+	tagKeyInstallationCxVersion          = tagKeyPrefix + dataKeyInstallationCxVersion
+	tagKeyInstallationCxCommand          = tagKeyPrefix + dataKeyInstallationCxCommand
+	tagKeyInstallationCxStatus           = tagKeyPrefix + dataKeyInstallationCxStatus
+	tagKeyInstallationCxConfigRepository = tagKeyPrefix + dataKeyInstallationCxConfigRepository
+	tagKeyInstallationOpenshiftNamespace = tagKeyPrefix + dataKeyInstallationOpenshiftNamespace
+	
+	metricNameCxInstallation = "cx_installer"
+)
+
+type MetricInstallation struct {
+	CxCommand          string
+	InstallStatus      string
+	CxConfigRepository string
+	TargetNameSpace    string
 }
 
-func (metric MetricGHProtection) Write() error {
-	influxClient := InfluxDB{}.Connect()
-	if influxClient != nil {
-		return nil
+func (metric MetricInstallation) WriteMetric() {
+	fillMetricInstallComponent(metric).WriteMetric()
+}
+
+func fillMetricInstallComponent(metric MetricInstallation) (metrics InfluxDB) {
+	metrics = InfluxDB{
+		MetricName: metricNameCxInstallation,
+		MetricsData: map[string]interface{}{
+			dataKeyInstallationCxVersion:          constants.CxVersion,
+			dataKeyInstallationCxCommand:          metric.CxCommand,
+			dataKeyInstallationCxStatus:           metric.InstallStatus,
+			dataKeyInstallationCxConfigRepository: metric.CxConfigRepository,
+			dataKeyInstallationOpenshiftNamespace: metric.TargetNameSpace,
+		},
+		Tags: map[string]string{
+			tagKeyInstallationCxVersion:          constants.CxVersion,
+			tagKeyInstallationCxCommand:          metric.CxCommand,
+			tagKeyInstallationCxStatus:           metric.InstallStatus,
+			tagKeyInstallationCxConfigRepository: metric.CxConfigRepository,
+			tagKeyInstallationOpenshiftNamespace: metric.TargetNameSpace,
+		},
 	}
-	return errors.New("mal was ausgeben")
+	return metrics
 }
